358c3df84e59751e3030b9c52dd5fa6e4efe7077
name-is-hai <haiqd065@gmail.com>
2024-03-27 10:32:07 +0700

    mail

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -4,7 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const domain = process.env.NEXT_PUBLIC_APP_URL;
 export const sendTwoFactorEmail = async (email: string, token: string) => {
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "NAMEISHAI <auth@nameishai.id.vn>",
     to: email,
     subject: "2FA Code",
     html: `<h1>Hello ${email}</h1><p>Your 2FA token: ${token}</p>`,
@@ -13,7 +13,7 @@ export const sendTwoFactorEmail = async (email: string, token: string) => {
 export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${domain}/auth/new-password?token=${token}`;
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "NAMEISHAI <auth@nameishai.id.vn>",
     to: email,
     subject: "Reset password",
     html: `<h1>Hello ${email}</h1><a href="${resetLink}">Click here to reset password</a>`,
@@ -24,7 +24,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
   const configLink = `${domain}/auth/new-verification?token=${token}`;
 
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "NAMEISHAI <auth@nameishai.id.vn>",
     to: email,
     subject: "Confirm your new verification email",
     html: `<h1>Hello ${email}</h1><a href="${configLink}">Click here to verify your email</a>`,
